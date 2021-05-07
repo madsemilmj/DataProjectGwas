6eeb9bd6a7019a77b95fafdb67960af9262ab0bc
MadsEmilMarkerJungersen <madsemil.mj@gmail.com>
2021-05-07 12:30:14 +0200

    testing

--- GWAS/R/MasterFunc.R
+++ GWAS/R/MasterFunc.R
@@ -32,8 +32,9 @@ MasterFunc <- function(total_indiv, indiv_chunk, SNP, h, c, k){
   ltfh_pheno_1 <- Assign_LTFH(Pheno_data = ny_true,
                               valT = k, h2 = h)
   print("LOLL")
-  lfth_pheno_1_selected <- ltfh_pheno_1 %>%
-    select(FID,IID,Pheno)
+  (lfth_pheno_1_selected <- ltfh_pheno_1 %>%
+    select(FID,IID,Pheno))
+
   fwrite(as.data.table(ltfh_pheno_1_selected),
          paste("./Pheno_LTFH","_",format(total_indiv,scientific = F),"_",format(SNP,scientific = F),"_",h*100,"_",k*100,"_1",".txt", sep=""),
          quote = F,
